33958e992719d8bb26cc8ff5bf27c6ab34d70689
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-24 19:23:40 +0100

    tests

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ConsoleApp1.Containers;
+
+Container container = new Container(100,1000,100,100,1000);
+LiquidContainer container2 = new LiquidContainer(100,1000,100,100,1000,true);
+CoolingContainer container10 = new CoolingContainer(100, 200, 300, 100, 100,PossibleProducts.Bananas,13.4);
+GasContainer container3 = new GasContainer(100,1000,100,100,1000,1000);
+
+
+Console.WriteLine(container.Name);
+Console.WriteLine(container2.Name);
+Console.WriteLine(container3.Name);
