d7b9201e05430a958517908883d55378cce23d54
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-24 19:22:31 +0100

    Added Interface to get property temperatures to products

--- ConsoleApp1/ConsoleApp1/Containers/PossibleProducts.cs
+++ ConsoleApp1/ConsoleApp1/Containers/PossibleProducts.cs
@@ -5,6 +5,7 @@ public enum PossibleProducts
     Bananas,
     Chocolate,
     Fish,
+    Meat,
     IceCream,
     FrozenPizza,
     Cheese,
@@ -12,4 +13,25 @@ public enum PossibleProducts
     Butter,
     Eggs
     
+}
+
+public interface PosibleProductsTemperature
+{
+    static private Dictionary<PossibleProducts, double> temperatures = new Dictionary<PossibleProducts, double>()
+    {
+        { PossibleProducts.Bananas, 13.3 },
+        { PossibleProducts.Chocolate, 18 },
+        { PossibleProducts.Fish, 2 },
+        { PossibleProducts.IceCream, -18 },
+        { PossibleProducts.FrozenPizza, -30 },
+        { PossibleProducts.Cheese, 7.2 },
+        { PossibleProducts.Sausages, 5 },
+        { PossibleProducts.Butter, 20.5 },
+        { PossibleProducts.Eggs, 19 }
+    };
+
+    static public double RequiredTemperature(PossibleProducts product)
+    {
+        return temperatures[product];
+    }
 }
