9d144f15d92f78d3ce77999edd4d34c965005778
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-24 17:05:16 +0100

    Added paramteters to Container.cs

--- ConsoleApp1/ConsoleApp1/Containers/Container.cs
+++ ConsoleApp1/ConsoleApp1/Containers/Container.cs
@@ -5,11 +5,25 @@ namespace ConsoleApp1.Containers;
 
 public class Container : IContainer
 {
+    public static int counter = 0;
     public double CargoWeight { get; set; }
-    
-    public Container(double cargoWeight)
+    public int Height { get; set; }
+    public int SelfWeight { get; set; }
+    public int Deep { get; set; }
+    public char Type { get; set; }
+    public int MaxLoad { get; set; }
+    public String Name { get; set; }
+
+    public Container(double cargoWeight,int height,int selfWeight, int deep, char type,int maxLoad)
     {
         CargoWeight = cargoWeight;
+        Height = height;
+        SelfWeight = selfWeight;
+        Deep = deep;
+        Type = type;
+        MaxLoad = maxLoad;
+        Name = "KON-" + Type + "-" + counter;
+        counter++;
     }
 
     public void Unload()
