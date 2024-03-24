13ec754fa22a6bf75272632cc1607c2858e3bb8f
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-24 17:13:25 +0100

    Add GasContainer.cs methods and logic

--- ConsoleApp1/ConsoleApp1/Containers/GasContainer.cs
+++ ConsoleApp1/ConsoleApp1/Containers/GasContainer.cs
@@ -1,6 +1,29 @@
+using ConsoleApp1.Interfaces;
+
 namespace ConsoleApp1.Containers;
 
-public class GasContainer
+public class GasContainer : Container,IHazardNotifier
 {
-    
+    public int Pressure { get; set; }
+
+    public GasContainer(double cargoWeight, int height, int selfWeight, int deep, char type, int maxLoad,int pressure) : base(cargoWeight, height, selfWeight, deep, type, maxLoad)
+    {
+        Pressure = pressure;
+    }
+
+    public void SendNotify(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public override void Load(double cargoWeight)
+    {
+        base.Load(cargoWeight);
+        
+    }
+
+    public override void Unload()
+    {
+        CargoWeight = CargoWeight * 0.05;
+    }
 }
