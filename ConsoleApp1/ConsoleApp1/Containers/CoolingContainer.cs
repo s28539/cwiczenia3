9317d41206810a32c620a86a1a7fa09287ddf9f1
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-24 19:21:01 +0100

    Add CoolingContainer.cs functions and logic

--- ConsoleApp1/ConsoleApp1/Containers/CoolingContainer.cs
+++ ConsoleApp1/ConsoleApp1/Containers/CoolingContainer.cs
@@ -1,6 +1,32 @@
+using ConsoleApp1.Exceptions;
+using ConsoleApp1.Interfaces;
+
 namespace ConsoleApp1.Containers;
 
-public class CoolingContainer
+public class CoolingContainer : Container , IHazardNotifier , PosibleProductsTemperature
 {
+    public PossibleProducts Product { get; set; }
+    public double Temperature { get; set; }
+
+    public CoolingContainer(double cargoWeight, int height, int selfWeight, int deep, int maxLoad, PossibleProducts product,double temperature) : base(cargoWeight, height, selfWeight, deep, maxLoad)
+    {
+        this.Temperature = temperature;
+        this.Product = product;
+        if (temperature != Containers.PosibleProductsTemperature.RequiredTemperature(product))
+        {
+            SendNotify(" Wrong Temperature for " + product); 
+        }
+        this.Type = 'C';
+        Name = "KON-" + Type + "-" + Container.counter;
+    }
     
+    public override void Load(double cargoWeight)
+    {
+        base.Load(cargoWeight);
+    }
+
+    public void SendNotify(string message)
+    {
+        Console.WriteLine(Name+message);
+    }
 }
