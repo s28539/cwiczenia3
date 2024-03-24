22968aea020f543ec03b54bef41c865a88d23b74
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-24 16:27:58 +0100

    Add methods to OverfillException.cs

--- ConsoleApp1/ConsoleApp1/Exceptions/OverfillException.cs
+++ ConsoleApp1/ConsoleApp1/Exceptions/OverfillException.cs
@@ -1,6 +1,16 @@
 namespace ConsoleApp1.Exceptions;
 
-public class OverfillException
+public class OverfillException : Exception
 {
-    
+    public OverfillException()
+    {
+    }
+
+    public OverfillException(string? message) : base(message)
+    {
+    }
+
+    public OverfillException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
