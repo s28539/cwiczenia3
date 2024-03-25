10db320cde08582b505d0b41ff92e2d4f4abf384
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-25 23:58:30 +0100

    tests again

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,12 +2,11 @@
 
 using ConsoleApp1.Containers;
 
-Container container = new Container(100,1000,100,100,1000);
-LiquidContainer container2 = new LiquidContainer(100,1000,100,100,1000,true);
-CoolingContainer container10 = new CoolingContainer(100, 200, 300, 100, 100,PossibleProducts.Bananas,13.4);
-GasContainer container3 = new GasContainer(100,1000,100,100,1000,1000);
+LiquidContainer liquidContainer = new LiquidContainer(200,1,1,1,1000,false);
+// liquidContainer.Load(701);
+// Console.WriteLine(liquidContainer.CargoWeight);
 
-
-Console.WriteLine(container.Name);
-Console.WriteLine(container2.Name);
-Console.WriteLine(container3.Name);
+GasContainer gasContainer = new GasContainer(100,1,1,1,1000,1000);
+gasContainer.Unload();
+Console.WriteLine(gasContainer.CargoWeight);
+gasContainer.Load(10000);
